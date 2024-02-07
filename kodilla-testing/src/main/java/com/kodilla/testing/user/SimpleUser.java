44f5576de782f615b35d92850de4aa07d08dc3cf
likeahim <michal.panko21@gmail.com>
2024-02-07 16:31:32 +0100

    6.2. adding unit test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.user;
+
+public class SimpleUser {
+
+    private String username;
+
+    public SimpleUser(String userName) {
+        this.username = userName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
