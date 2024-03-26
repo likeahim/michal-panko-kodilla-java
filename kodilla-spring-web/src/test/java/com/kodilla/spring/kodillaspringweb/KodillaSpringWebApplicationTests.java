7c7f17796bde703f19385d8e791826e01a60df10
likeahim <michal.panko21@gmail.com>
2024-03-26 16:32:51 +0100

    13.4. Spring Web

---
+++ kodilla-spring-web/src/test/java/com/kodilla/spring/kodillaspringweb/KodillaSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.kodillaspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
