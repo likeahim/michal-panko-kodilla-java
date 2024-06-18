dc259b6eb342304cc498d595d5a479cecbbefc51
likeahim <michal.panko21@gmail.com>
2024-06-18 15:32:44 +0200

    23.3. Aspects

--- kodilla-good-patterns/src/test/java/com/kodilla/patterns2/facade/ShopServiceTest.java
+++ kodilla-good-patterns/src/test/java/com/kodilla/patterns2/facade/ShopServiceTest.java
@@ -1,5 +1,9 @@
 package com.kodilla.patterns2.facade;
 
+import com.kodilla.patterns2.facade.api.ItemDto;
+import com.kodilla.patterns2.facade.api.OrderDto;
+import com.kodilla.patterns2.facade.api.OrderFacade;
+import com.kodilla.patterns2.facade.api.OrderProcessingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +14,8 @@ import java.math.BigDecimal;
 class ShopServiceTest {
     @Autowired
     private ShopService shopService;
+    @Autowired
+    private OrderFacade orderFacade;
     @Test
     void testShopServiceSubmitOrder() {
         long orderId = shopService.openOrder(1L);
@@ -51,6 +57,20 @@ class ShopServiceTest {
             }
         } else {
             System.out.println("Access denied. User is not authenticated.");
+        }
+    }
+
+    @Test
+    void testShopFacade() {
+        OrderDto order = new OrderDto();
+        order.addItem(new ItemDto(10L, 2));
+        order.addItem(new ItemDto(216L, 1));
+        order.addItem(new ItemDto(25L, 1));
+        order.addItem(new ItemDto(11L, 3));
+        try {
+            orderFacade.processOrder(order, 1L);
+        } catch (OrderProcessingException e) {
+            // business exception - should be handled in real application
         }
     }
 }
